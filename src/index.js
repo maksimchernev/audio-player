5af933f141f6d6c53fbbdb2b5fbca15a37b642cd
Maksim Chernev <m.chernev@mail.ru>
2023-02-28 23:44:01 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/index.css';
-import './assets/styles/responsive.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
